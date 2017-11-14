90586a9b84900611b65882f8024824752e2e5b27
Killian Beguerie <killian.beguerie@e-artsup.net>
2017-11-14 12:41:07 +0100

    d

--- Assets/Scripts/Move_Manager.cs
+++ Assets/Scripts/Move_Manager.cs
@@ -4,13 +4,50 @@ using UnityEngine;
 
 public class Move_Manager : MonoBehaviour {
 
-	// Use this for initialization
+    private float MaxSpeed = 100f;
+
+	private Rigidbody rb;
+    private float ForwardAcceleration = 20f;
+    private float StraffMaxSpeed = 100f;
+    private float smoothXVelocity;
+    private float StraffTime = 0.1f;
+
+    // Use this for initialization
+    private void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+		AddMove();
 	}
+
+	void AddMove()
+	{
+        Vector3 newVelocity = rb.velocity;
+        if(newVelocity.z > MaxSpeed)
+        {
+            newVelocity.z = MaxSpeed;
+        }
+        else
+        {
+            newVelocity.z += ForwardAcceleration * Time.fixedDeltaTime;
+        }
+        float targetVelocity = Input.GetAxis("Horizontal") * StraffMaxSpeed;
+
+        newVelocity.x = Mathf.SmoothDamp(newVelocity.x, targetVelocity, ref smoothXVelocity, StraffTime);
+
+        rb.velocity = newVelocity;
+
+        
+	}
+
+    private void LateUpdate()
+    {
+        Debug.Log(rb.velocity.z);
+    }
 }
